e4c890c90cbbefe410bbeb97211edd2df6b25856
ravi <ravi2001pandey@gmail.com>
2025-03-19 00:05:19 +0530

    Fix: Changed layout for responsive design

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from 'react-router-dom'
+import axios from "axios";
 
 const Content = () => {
   const [listItems, setListItems] = useState(null);
@@ -12,31 +13,64 @@ const Content = () => {
     if (!listItems) handleFetch();
     if (dummyData === null) setDummyData(listItems);
     if (search != "") {
-      const filteredList = listItems.filter((item) => {
-        if (item.info.name.toLowerCase().includes(search.toLowerCase()))
-          return item;
-      });
-      setDummyData(filteredList);
+      handleFetch()
     } else setDummyData(listItems);
   }, [search, listItems]);
 
   const handleFetch = async () => {
-    const req = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const data = await req.json();
-    const obj =
-      data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants;
-    setListItems(obj);
 
-    // Currently not required.
-    // const reqData = Object.values(obj);
+    // URL for getting random food items data from The Meal DB API.
+    // const fetchRandomMealURL = `${process.env.REACT_APP_FETCH_RANDOM_FOOD_ITEMS}`
+
+    // const searchUrl = `${process.env.REACT_APP_SEARCH_MEAL_BY_FIRST_LETTER}${firstLetter}`;
+    let mappedData = []
+    if (search.trim().length < 1) {
+      const fetchRandomMealURL = `${process.env.REACT_APP_FETCH_RANDOM_FOOD_ITEMS}`;
+      console.log("fetchRandomMealURL", fetchRandomMealURL)
+
+      const mealPromises = []
+      for (let i = 1; i <= 15; i++) {
+        mealPromises.push(axios.get(fetchRandomMealURL))
+      }
+
+      const uniqueIds = new Set();
+      const responses = await Promise.all(mealPromises);
+
+      mappedData = responses.reduce((acc, response) => {
+        const meal = response?.data?.meals?.[0];
+        if (meal && !uniqueIds.has(meal.idMeal)) {
+          uniqueIds.add(meal.idMeal);
+          acc.push(meal);
+        }
+        return acc;
+      }, []);
+      setListItems(mappedData);
+
+    } else {
+      const firstLetter = search[0];
+
+      const searchUrl = `${process.env.REACT_APP_SEARCH_MEAL_BY_FIRST_LETTER}${firstLetter}`
+      console.log("searchUrl", searchUrl)
+      mappedData = (await axios.get(searchUrl))?.data?.meals;
+
+      setDummyData(mappedData)
+    }
+  };
+
+  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const handleClick = (letter) => {
+    console.log("letter", letter)
+    if (letter == search) {
+      setSearch("")
+    }
+    else {
+      setSearch(letter)
+    }
   };
 
   return (
     <>
-      <div className="searchBox">
+      {/* <div className="searchBox">
         <input
           className="searchInput"
           type="text"
@@ -54,17 +88,37 @@ const Content = () => {
           here
         </button>
 
+      </div> */}
+      <div>
+        <div className="container">
+          <h2 className="title"> Filter Dishes Based on first Alphabet of the food name!!</h2>
+          <div className="alphabet-container">
+            {alphabets.map((letter) => (
+              <button
+                key={letter}
+                onClick={() => handleClick(letter)}
+                className={`alphabet-button ${search === letter ? "selected" : ""}`}
+              >
+                {letter}
+              </button>
+            ))}
+          </div>
+
+        </div>
       </div>
-      { dummyData == null && <Shimmer />}
+      {dummyData == null && <Shimmer />}
       <div className="content">
-        { dummyData != null &&
+        {dummyData != null &&
           dummyData.map((item) => {
             // const reqURL = "https://www.swiggy.com/restaurants/";
-            // const foodID = (item?.cta?.link).slice(reqURL.length,item?.cta?.link.length);            
+            // const foodID = (item?.cta?.link).slice(reqURL.length,item?.cta?.link.length); 
+
             return (
-              <Link to={ item.cta.link } key={item.info.id} link={item}> <RestaurantCard
-                { ...item.info }
-            /> </Link>
+              // <Link to={item.cta.link} key={item.info.id} link={item}> <RestaurantCard
+              //   {...item.info}
+              // /> </Link>
+
+              <Link key={item.idMeal} > <RestaurantCard {...item} /></Link>
             );
           })}
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,29 +1,30 @@
 import React from "react";
 
-const RestaurantCard = ({ cloudinaryImageId, name, avgRating, sla, cuisines }) => {
-  let cuisinesList = cuisines[0];
-  for (let i = 1; i < cuisines.length; i++) {
-    if (cuisinesList.length + cuisines[i].length > 20)
-      {
-        break;
-      }
+const RestaurantCard = ({ idMeal, strMeal, strArea, strMealThumb }) => {
 
-    cuisinesList +=  "," + cuisines[i];
-  }
-  cuisinesList += "...";
+  // let cuisinesList = cuisines[0];
+  // for (let i = 1; i < cuisines.length; i++) {
+  //   if (cuisinesList.length + cuisines[i].length > 20)
+  //     {
+  //       break;
+  //     }
+
+  //   cuisinesList +=  "," + cuisines[i];
+  // }
+  // cuisinesList += "...";
 
   return (
     <div className="card">
       <div className="imgDiv">
-      <img
-        className="foodImage"
-        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
-        alt="img here"
-      />
+        <img
+          className="foodImage"
+          src={strMealThumb}
+          alt="Food image here"
+        />
       </div>
-      <h3>{name} </h3>
-      <h4> * { avgRating } .{ sla.slaString } </h4>
-      <h4> {cuisinesList}</h4>
+      <h5>{strMeal} - { strArea }</h5>
+      {/* <h4> * {strArea}</h4> */}
+      {/* <h4> {cuisinesList}</h4> */}
     </div>
   );
 };
