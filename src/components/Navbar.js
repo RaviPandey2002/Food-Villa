c25e9f906bda11cb56798cf3985125f957b42716
ravi <ravi2001pandey@gmail.com>
2025-03-22 15:41:17 +0530

    Changed package.jason for manual addition of assets for logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "../../public/Image/Food-Villa.png";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -9,17 +8,17 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <img className="logo" src={logo} alt="Food Villa" />
+      <img className="logo" src="/assets/logo.png" alt="Food Villa" />
       <ul className="navLinks">
         <Link to="/">Home</Link>
         <Link to="/about">About Us</Link>
         <Link to="/Cart">My Orders</Link>
         {!isLoggedIn && (
-            <Link to="/signUp"> 
-            <button 
-              onClick={(e)=>{  e.target.value="LoggedIn"; setStatus("LogIn")}}
-            > { status }</button>
-            </Link>
+          <Link to="/signUp">
+            <button
+              onClick={(e) => { e.target.value = "LoggedIn"; setStatus("LogIn") }}
+            > {status}</button>
+          </Link>
         )}
       </ul>
     </div>
